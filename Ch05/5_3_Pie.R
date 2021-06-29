bfc368dc4e85634dd84910c95a923abeec25e270
chhak2021 <chhak2021@gmail.com>
2021-06-29 17:44:39 +0900

    2021/06/29 Ch05 데이터 시각화 예제실습

---
+++ Ch05/5_3_Pie.R
@@ -0,0 +1,16 @@
+#날짜 : 2021/06/29
+#이름 : 김철학
+#내용 : Ch05.데이터 시각화 - 파이차트 교재 p147
+
+season <- c('winter', 'summer', 'spring', 'summer', 'summer',
+            'autumn', 'autumn', 'summer', 'spring', 'spring')
+
+season
+ds <- table(season)
+ds
+
+
+pie(ds, main = 'Season')
+pie(ds, main = 'Season', col = terrain.colors(4))
+
+#교재 p148 실습 - 분기별 매출현황을 파이 차트로 시각화하기
