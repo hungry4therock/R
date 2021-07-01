06c5150c30a59c88aafe1a1f3805822ecaa54883
chhak2021 <chhak2021@gmail.com>
2021-07-01 17:19:59 +0900

    2021/07/07 Ch09 수업예제

--- Ch09/9_2_WordCloud.R
+++ Ch09/9_2_WordCloud.R
@@ -23,9 +23,28 @@ library(wordcloud)
 txt_data <- readLines('./file/wordcloud_data.txt', encoding='UTF-8')
 txt_data
 
+#명사 추출
+txt_noun <- extractNoun(txt_data)
+txt_noun
+
+#명사 단어 집계
+txt_noun_count <- table(unlist(txt_noun))
+txt_noun_count
+
+#데이터프레임 변환
+df_txt <- as.data.frame(txt_noun_count, stringsAsFactors = F)
+View(df_txt)
+
 
 #단어구름 시각화
-#wordcloud(words = txt_data)
+wordcloud(words = df_txt$Var1,
+          freq  = df_txt$Freq,
+          min.freq = 1,
+          max.words = 1000,
+          random.order = F,
+          rot.per = 0.1,
+          scale = c(4, 0.3),
+          colors = brewer.pal(12, 'Paired'))
 
 
 
